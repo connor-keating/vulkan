e02e6fd83dd6a60807a090447918368835c80eae
Connor <conkeating32@gmail.com>
2025-01-25 16:10:11 -0500

    Removing unused DLL compile command.

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 @echo off
 
 set TurnOffWarnings= -wd4189 -wd4201
-set CommonCompilerFlags=-Zi -nologo -W4 %TurnOffWarnings% 
+set CommonCompilerFlags=-std:c11 -Zi -nologo -W4 %TurnOffWarnings% 
 @REM Add build macros
 set CommonCompilerFlags= -D DEBUG_BUILD=1 %CommonCompilerFlags%
-set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib opengl32.lib
+set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib 
 
 
 IF NOT EXIST .build mkdir .build
@@ -29,14 +29,8 @@ set timestamp=%year%%month%%day%_%hour%%minute%%second%
 set namePDB=application_%timestamp%.pdb
 
 @REM 64-bit build command
-call cl %CommonCompilerFlags% ..\src\application.c -Fmhandmade.map -Foapplication.obj -Fd%namePDB% -LD -link -incremental:no -opt:ref -PDB:%namePDB% opengl32.lib 
-@REM call cl /c /Zi /nologo src\application.c /Foapplication.obj 
-@REM call link opengl32.lib /nologo /DLL /DEBUG /OUT:application.dll application.obj
-call cl %CommonCompilerFlags% %includes% %files% -link %CommonLinkerFlags% -OUT:dungeon.exe
-
-@REM Example external hot reload.
-@REM cl %CommonCompilerFlags% ..\src\external.cpp /LD /link -PDB:external.pdb /EXPORT:MyExport
-@REM cl %CommonCompilerFlags% /I ..\src ..\src\win32_main.cpp /link %CommonLinkerFlags%
+@REM call cl %CommonCompilerFlags% ..\src\application.c -Fmhandmade.map -Foapplication.obj -Fd%namePDB% -LD -link -incremental:no -opt:ref -PDB:%namePDB% opengl32.lib 
+call cl %CommonCompilerFlags% %includes% %files% -link %CommonLinkerFlags% -OUT:main.exe
 
 
 popd
