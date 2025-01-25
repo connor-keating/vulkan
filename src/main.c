d2351515ddcfb255236473bc57bf65eb8f29d367
Connor <conkeating32@gmail.com>
2025-01-25 16:15:35 -0500

    Switching to win32 main for opening a window.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,19 @@
+#pragma once
+
+#pragma warning(push, 0)
+#include <windows.h>
 #include <stdio.h>
+#pragma warning(pop)
 
 #include "core.c"
 
-int main()
+int WINAPI WinMain(HINSTANCE currentInstanceHandle, HINSTANCE prevInstanceHandle, PSTR argsCommandLine, int displayFlag)
 {
+    UNREFERENCED_PARAMETER(currentInstanceHandle);
+    UNREFERENCED_PARAMETER(prevInstanceHandle);
+    UNREFERENCED_PARAMETER(argsCommandLine);
+    UNREFERENCED_PARAMETER(displayFlag);
+
     printf("Hello, world\n");
     return 0;
 }
