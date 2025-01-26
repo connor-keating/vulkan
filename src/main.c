ed5dca9b62464fa6015be0ad5e1ba84b1ff53ca7
Connor <conkeating32@gmail.com>
2025-01-26 10:34:15 -0500

    Allocating a GB of memory for the application to use.

--- src/main.c
+++ src/main.c
@@ -15,6 +15,17 @@ int WINAPI WinMain(HINSTANCE currentInstanceHandle, HINSTANCE prevInstanceHandle
     UNREFERENCED_PARAMETER(argsCommandLine);
     UNREFERENCED_PARAMETER(displayFlag);
 
+    // Start up section
+
+    // Allocate program memory
+    // Allocate application memory
+    LPVOID app_memory_base = 0;
+    size_t app_memory_size = (size_t) Gigabytes(1);
+    void *app_memory = VirtualAlloc(app_memory_base, app_memory_size, MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
+    ASSERT(app_memory, "ERROR: Unable to allocate application memory.");
+    arena root_arena = {0};
+    arena_init(&root_arena, app_memory, app_memory_size);
+     
     printf("Hello, world\n");
     #if defined(_WIN64)
         printf("Compiled for x64.\n");
